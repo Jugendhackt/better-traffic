bb94bca411bdb0c5cba8532b47be88a76c3f874a
Maximtap <maxim@grueningeronline.de>
2016-06-11 11:32:00 +0000

    Add files via upload

--- Ampel.py
+++ Ampel.py
@@ -12,11 +12,32 @@ c.pack()
 Ampelliste = list()
 Autoliste = list()
 Autorichtung = list()
-def erstelleAuto(x, y):
-    Autografik = c.create_rectangle(x-15, y-15, x+15, y+15, fill="blue")
-    Autoliste.append(Autografik)
-def bewegeAuto(Auto):
-    pass
+def erstelleAuto(Richtung):
+    if Richtung == "oben":
+        Autografik = c.create_rectangle(485, 0, 515, 30, fill="blue")
+        Autoliste.append(Autografik)
+    elif Richtung == "rechts":
+        Autografik = c.create_rectangle(970, 385, 1000, 415, fill="blue")
+        Autoliste.append(Autografik)
+    elif Richtung == "unten":
+        Autografik = c.create_rectangle(485, 770, 515, 800, fill="blue")
+        Autoliste.append(Autografik)
+    elif Richtung == "links":
+        Autografik = c.create_rectangle(0, 385, 30, 415, fill="blue")
+        Autoliste.append(Autografik)
+    window.update()
+    Autorichtung.append(Richtung)
+def bewegeAutos():
+    for i in range(len(Autoliste)):
+        if Autorichtung[i] == "oben":
+            c.move(Autoliste[i], 0, 5)
+        elif Autorichtung[i] == "rechts":
+            c.move(Autoliste[i], -5, 0)
+        elif Autorichtung[i] == "unten":
+            c.move(Autoliste[i], 0, -5)
+        elif Autorichtung[i] == "links":
+            c.move(Autoliste[i], 5, 0)
+        window.update()
 def erstelleAmpel(x, y):
     Ampelgrafik = c.create_oval(x-15, y-15, x+15, y+15, fill="red")
     Ampelliste.append(Ampelgrafik)
@@ -29,8 +50,17 @@ erstelleAmpel(600, 500)
 erstelleAmpel(600, 300)
 erstelleAmpel(400, 500)
 erstelleAmpel(400, 300)
-erstelleAuto(300, 300)
 window.update()
+
+erstelleAuto("oben")
+erstelleAuto("unten")
+erstelleAuto("rechts")
+erstelleAuto("links")
+
+for i in range(0, 100, 1):
+    bewegeAutos()
+    sleep(0.1)
+
 
 
         

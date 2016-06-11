461cb5d9613d130a80c92ee054967f61fd5853f6
Maximtap <maxim@grueningeronline.de>
2016-06-11 15:02:28 +0000

    Add files via upload
    Geuploaded um 16 Uhr!

--- Ampel.py
+++ Ampel.py
@@ -1,4 +1,4 @@
-from Tkinter import *
+from tkinter import *
 from random import randint
 from time import sleep, time
         
@@ -16,7 +16,6 @@ Autorichtung = list()
 
 class Auto:
 
-    # local variables of the car
     size = 30
     xord = 0
     yord = 0
@@ -24,29 +23,39 @@ class Auto:
     yvel = 0
     gfx  = None
 
-    # initiating with the upper left coordinate and the canvas
     def __init__(self, x, y, c):
         self.xord = x
         self.yord = y
         self.gfx  = c.create_rectangle(x, y, x + self.size, y + self.size, fill="blue")
 
-    # accelerating the car in this direction
     def accelerate(self, x, y):
         self.xvel += x
         self.yvel += y
 
-    # letting the car drive
     def drive(self):
         self.xord += self.xvel
         self.yord += self.yvel
 
-    # drawing the car on the canvas
     def draw(self, c):
         c.coords(self.gfx, self.xord, self.yord, self.xord + self.size, self.yord + self.size)
 
+class Ampel:
+    size = 30
+    xord = 0
+    yord = 0
+    circle = None
 
-# creates a car, in possibly one of the four directions:
-#   "oben", "rechts", "unten", "links"
+    def __init__(self, x, y, f, c):
+        self.xord = x
+        self.yord = y
+        self.circle = c.create_oval(x - (self.size / 2), y - (self.size / 2), x + (self.size / 2), y + (self.size / 2), fill = f)
+        
+
+    def Ampelfarbe(self, farbe):
+        pass
+        
+    
+    
 def erstelleAuto(Richtung):
     if Richtung == "oben":
         a = Auto(485, 0, c)
@@ -67,29 +76,26 @@ def erstelleAuto(Richtung):
     window.update()
     Autorichtung.append(Richtung)
 
-# moving all the prior created cars and redraws them
 def bewegeAutos():
     for i in range(len(Autoliste)):
         Autoliste[i].drive()
         Autoliste[i].draw(c)
     window.update()
 
-def erstelleAmpel(x, y):
-    Ampelgrafik = c.create_oval(x-15, y-15, x+15, y+15, fill="red")
-    Ampelliste.append(Ampelgrafik)
-
 def aenderAmpel(Farbe, Ampelgrafik):
     c.itemconfig(Ampelgrafik, fill = Farbe)
 
-
-
-# debugging world
 Strasse1 = c.create_line(500, 0, 500, 800)
 Strasse2 = c.create_line(0, 400, 1000, 400)
-erstelleAmpel(600, 500)
-erstelleAmpel(600, 300)
-erstelleAmpel(400, 500)
-erstelleAmpel(400, 300)
+
+A = Ampel(600, 500, "green", c)
+Ampelliste.append(A)
+A = Ampel(600, 300, "green", c)
+Ampelliste.append(A)
+A = Ampel(400, 500, "green", c)
+Ampelliste.append(A)
+A = Ampel(400, 300, "green", c)
+Ampelliste.append(A)
 
 erstelleAuto("oben")
 erstelleAuto("unten")
@@ -100,8 +106,12 @@ window.update()
 
 for i in range(0, 100, 1):
     bewegeAutos()
-    sleep(0.01)
+    sleep(0.05)
 
+
+
+
+        
 
 
 

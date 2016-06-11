c244c64c024883983b1a781d4aeb63a06706f1d9
ANewDeviloper <teufel.timo6@gmail.com>
2016-06-11 17:32:40 +0200

    Kommentare eingefügt

--- Control_Panel.py
+++ Control_Panel.py
@@ -1,10 +1,11 @@
 from Tkinter import *
 from random import randint
 from time import sleep, time
-        
+
+#Diese Methode stellt die Richtung des Fahrzeuges ein
+       
 global Flag
 Flag = 1
-
 
 def ChangeDirection():
     global Flag
@@ -13,10 +14,14 @@ def ChangeDirection():
     else:
         DirectionCarShow.config(text="A zu B")
     Flag = -Flag
+
+#Aufbau des Fensters
     
 ControlPanel = Tk()
 ControlPanel.geometry("300x200")
 ControlPanel.title("Control Panel")
+
+#Labes im Fenster
 
 CarHeadline = Label(ControlPanel, justify = "center", text = "Create a Car:")
 CarHeadline.place(x = 10, y =20, width = 75, height = 25)
@@ -39,8 +44,7 @@ HumanGroup.place(x = 180, y = 130, width = 50, height = 25)
 DirectionCarShow = Label(ControlPanel, justify = "center", text="A zu B")
 DirectionCarShow.place(x = 70, y = 75, width = 50, height = 25)
 
-
-
+#Entrys im Fenster
 
 CarGroupSelect = Entry(ControlPanel)
 CarGroupSelect.place(x = 75, y = 130, width = 25, height = 25)
@@ -48,9 +52,7 @@ CarGroupSelect.place(x = 75, y = 130, width = 25, height = 25)
 HumanGroupSelect = Entry(ControlPanel)
 HumanGroupSelect.place(x = 245, y = 130, width = 25, height = 25)
 
-
-
-
+#Buttons im Fenster
 
 CreateCarButton = Button(ControlPanel, text = "Create", command = <ERSTELLE AUTO>)
 CreateCarButton.place(x = 25, y = 50, width = 75, height = 25)
@@ -60,7 +62,6 @@ CreateHumanButton.place(x = 200, y =50, width = 75, height = 25)
 
 ChangeCarDirectionButton = Button(ControlPanel, text = "Change", command = ChangeDirection)
 ChangeCarDirectionButton.place(x = 25, y = 100, width = 75, height = 25)
-
 
 
 ControlPanel.mainloop()

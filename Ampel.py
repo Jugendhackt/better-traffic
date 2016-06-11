edd311af5c10f4adb83082d9edbf90a25d6fe27f
blueburningcoder <f.karg10@gmail.com>
2016-06-11 17:15:21 +0200

    Ampel is a class now

--- Ampel.py
+++ Ampel.py
@@ -95,33 +95,48 @@ def bewegeAutos():
         Autoliste[i].draw(c)
     window.update()
 
-def erstelleAmpel(x, y):
-    Ampelgrafik = c.create_oval(x-15, y-15, x+15, y+15, fill="red")
-    Ampelliste.append(Ampelgrafik)
 
-def aenderAmpel(Farbe, Ampelgrafik):
-    c.itemconfig(Ampelgrafik, fill = Farbe)
+class Ampel:
+    size = 30
+    xord = 0
+    yord = 0
+    farb = "rot"
+    circ = None
 
+    # initializing the Ampel at given coordinates with fill (=f) on the canvas (=c)
+    def __init__(self, x, y, f, c):
+        self.xord = x
+        self.yord = y
+        self.circ = c.create_oval(x - (self.size / 2), y - (self.size / 2), x + (self.size / 2), y + (self.size / 2), fill = f)
+
+    def setAmpelfarbe(self, farbe, c):
+        c.itemconfig(circ, fill=farbe)
+        self.farb = farbe
+
+    def getAmpelfarbe(self):
+        return self.farb
 
 
 # debugging world
 Strasse1 = c.create_line(500, 0, 500, 800)
 Strasse2 = c.create_line(0, 400, 1000, 400)
-erstelleAmpel(600, 500)
-erstelleAmpel(600, 300)
-erstelleAmpel(400, 500)
-erstelleAmpel(400, 300)
 
 erstelleAuto("oben")
 erstelleAuto("unten")
 erstelleAuto("rechts")
 erstelleAuto("links")
 
+Ampelliste.append(Ampel(600, 500, "green", c))
+Ampelliste.append(Ampel(600, 300, "green", c))
+Ampelliste.append(Ampel(400, 500, "green", c))
+Ampelliste.append(Ampel(400, 300, "green", c))
+
+
 window.update()
 
 for i in range(0, 1500, 1):
     bewegeAutos()
-    sleep(0.01)
+    sleep(0.02)
 
 
 

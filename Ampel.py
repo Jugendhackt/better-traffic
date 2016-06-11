96280e097947126b290316fa7091f0cda8beb0ca
blueburningcoder <f.karg10@gmail.com>
2016-06-11 23:50:07 +0200

    End of Day

--- Ampel.py
+++ Ampel.py
@@ -4,6 +4,9 @@ from time import sleep, time
 from math import *
 
 
+DEBUG = False
+
+
 HEIGHT = 800
 WIDTH = 1000
 window = Tk()
@@ -16,19 +19,20 @@ Autoliste = list()
 Autorichtung = list()
 
 magicEvents = list()
+magicPoints = list()
 
 class Events:
     TurnRight, TurnLeft, LightStop, LightGo, StopCar, GoCar = range(6)
 
-class MagicEvent:
+class MagicEvent(object):
     size  = 30
     xord  = 0
     yord  = 0
     event = None
 
+    # initializing the derived object
     def __init__(self):
         magicEvents.append(self)
-        pass
 
     def getEvent(self):
         return event
@@ -37,18 +41,25 @@ class MagicEvent:
         self.event = e
 
     def getPos(self):
-        return (xord, yord)
+        return (self.xord, self.yord)
+
+    # basically a simply virtual function
+    def eventHappening(self, event):
+        pass
 
 
-class MagicPoint:
+class MagicPoint(object):
     xloc = 0
     yloc = 0
+    aref = None
 
-    def __init__(self):
-        pass
 
-    def eventHappening(event):
-        pass
+    def __init__(self, ref):
+        magicPoints.append(self)
+        self.aref = ref
+
+    def eventHappening(self, event):
+        self.aref.eventHappening(event)
 
     # moving this MagicPoint
     def move(self, x, y):
@@ -56,7 +67,8 @@ class MagicPoint:
         self.yloc += y
 
     # setting the location of this MagicPoint
-    def setPoint(self, x, y):
+    def setPosition(self, x, y):
+        if DEBUG: print "set position of MP to %d %d" % (x, y)
         self.xloc = x
         self.yloc = y
 
@@ -66,13 +78,18 @@ class MagicPoint:
 
     # checking if the point is within an event-trigger
     def checkForEvent(self):
+        if DEBUG: print "-- -- Magic Point at x:" + str(self.xloc) + " y:" + str(self.yloc)
         for e in magicEvents:
             (ex, ey) = e.getPos()
-            if  self.xloc + 7  >= ex \
-              & self.xloc - 7  <= ex \
-              & self.yloc + 7  >= ey \
-              & self.yloc - 7  <= ey:
-                eventHappening(e.getEvent())
+            if DEBUG: print "Magic Event at x:" + str(ex) + " y:" + str(ey)
+            if  (self.xloc + 8  >= ex) \
+              & (self.xloc - 8  <= ex) \
+              & (self.yloc + 8  >= ey) \
+              & (self.yloc - 8  <= ey):
+                  if DEBUG: print "Event triggered"
+                  self.eventHappening(e.getEvent())
+                  pass
+        self.eventHappening(Events.GoCar)
 
 
 
@@ -102,40 +119,49 @@ class Auto(MagicEvent):
     xvel = 0
     yvel = 0
     gfx  = None
-    mpoi = list()
+    mpoi = None
     dire = None
+    leve = Events.GoCar
 
     # initiating with the upper left coordinate and the canvas
     def __init__(self, x, y, c):
+        super(Auto, self).__init__()
         self.xord = x
         self.yord = y
         self.gfx  = c.create_rectangle(x, y, x + self.size, y + self.size, fill="blue")
         self.setEvent(Events.StopCar)
-        p = MagicPoint()
-        self.mpoi.append(p)
+        self.mpoi = MagicPoint(self)
 
     # returns the current position of the car
     def getPosition(self):
-        return (xord, yord)
+        return (self.xord, self.yord)
 
     # accelerating the car in this direction
     def accelerate(self, x, y):
         self.xvel += x
         self.yvel += y
 
-    # absolutely stops the car
+    # absolutely stops the car and saves previous speed
     def stop(self):
+        if DEBUG: print "Stopping Car."
         self.dire = (self.xvel, self.yvel)
         self.xvel = 0
         self.yvel = 0
 
     def go(self):
+        if DEBUG: print "restarted Car."
         self.xvel, self.yvel = self.dire
 
     # letting the car drive
     def drive(self):
-        self.xord += self.xvel
-        self.yord += self.yvel
+        if (self.leve == Events.LightStop) | (self.leve == Events.StopCar):
+            pass
+        else:
+            self.xord += self.xvel
+            self.yord += self.yvel
+            if (self.xvel != 0) | (self.yvel != 0):
+                self.mpoi.setPosition(self.xord + 3 * self.xvel, self.yord + 3 * self.yvel)
+                if DEBUG: print "New MP-Position: %d %d" % (self.xord + 3 * self.xvel, self.yord + 3 * self.yvel)
 
     # drawing the car on the canvas
     def draw(self, c):
@@ -145,11 +171,27 @@ class Auto(MagicEvent):
         c.coords(self.gfx, nxord, nyord, nxord + self.size, nyord + self.size)
 
     # In case an event got triggered
-    def eventHappening(event):
-        if event == Events.LightStop | event == Events.StopCar:
+    def eventHappening(self, event):
+        if (event == Events.LightStop) | (event == Events.StopCar):
+            if DEBUG: print "gotta stop car"
             self.stop()
-        if event == Events.LightGo:
-            self.accelerate(0,-5)
+        elif event == Events.LightGo:
+            if DEBUG: print "gotta restart car"
+            self.go()
+        elif event == Events.TurnLeft:
+            pass
+        elif event == Events.TurnRight:
+            pass
+        elif event == Events.GoCar:
+            pass
+        # else:
+        #     event = Events.GoCar
+        self.leve = event
+        if DEBUG: print "Event happened: " + str(event)
+
+
+    def getEvent(self):
+        return Events.StopCar
 
 
 
@@ -170,14 +212,10 @@ def erstelleAuto(x, y, Richtung):
     Autorichtung.append(Richtung)
 
 
-# moving all the prior created cars and redraws them, as well as redraws the Ampels ... ?
-def bewegeAutos():
-    for auto in Autoliste:
-        auto.drive()
-        auto.draw(c)
-    for ampel in Ampelliste:
-        ampel.draw(c)
-    window.update()
+def create():
+    if DEBUG: print "create car on the right side"
+    erstelleAuto(970, 385, "rechts")
+
 
 
 
@@ -190,13 +228,14 @@ class Ampel(MagicEvent):
 
     # initializing the Ampel at given coordinates with fill (=f) on the canvas (=c)
     def __init__(self, x, y, f, c):
+        super(Ampel, self).__init__()
         self.xord = x
         self.yord = y
         self.circ = c.create_oval(x - (self.size / 2), y - (self.size / 2), x + (self.size / 2), y + (self.size / 2), fill = f)
 
     # setting the color of the Ampel and drawing it on the canvas
     def setAmpelfarbe(self, farbe, c):
-        c.itemconfig(circ, fill=farbe)
+        c.itemconfig(self.circ, fill=farbe)
         self.farb = farbe
 
     # returns the current state or color of the Ampel
@@ -228,10 +267,6 @@ erstelleAuto("rechts")
 erstelleAuto("links")
 
 
-def create():
-    erstelleAuto("oben")
-
-
 Ampelliste.append(Ampel(515, 500, "green", c))
 Ampelliste.append(Ampel(600, 385, "green", c))
 Ampelliste.append(Ampel(400, 415, "green", c))
@@ -253,9 +288,25 @@ CreateCarButton = Button(window, text = "Create", command = create)
 CreateCarButton.place(x = 25, y = 50, width = 75, height = 25)
 
 
+# moving all the prior created cars and redraws them, as well as redraws the Ampels ... ?
+def loop(tick):
+    if DEBUG: print "--- --- ---   --- ---- Tick " + str(tick)
+    for auto in Autoliste:
+        auto.drive()
+        auto.draw(c)
+    for ampel in Ampelliste:
+        ampel.draw(c)
+    if DEBUG: print "amount of magic points: " + str(len(magicPoints))
+    if DEBUG: print "amount of magic events: " + str(len(magicEvents))
+    for point in magicPoints:
+        point.checkForEvent()
+    window.update()
+
+
+
 
 for i in range(0, 1500, 1):
-    bewegeAutos()
+    loop(i)
     sleep(0.04)
 
 
